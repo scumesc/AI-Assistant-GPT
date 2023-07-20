5e14291ea0f1215f5b042cfd4ebd69d266bdfee5
scumesc <scumesc@scumesc-2.local>
2023-07-20 07:12:00 +0500

    initiall commmit

--- backend/urls.py
+++ backend/urls.py
@@ -5,6 +5,8 @@ from chatapp.views import index
 from chatapp.views import process_json
 from chatapp.prompt import process_json2
 
+from chatapp.views import companyinfo
+
 
 urlpatterns = [
     path('', index, name='index'),
@@ -12,5 +14,7 @@ urlpatterns = [
 
     path('process_json/', process_json, name='process_json'),
     path('process_json2/', process_json2, name='process_json2'),
+
+    path('companyinfo/', companyinfo, name='companyinfo'),
 
 ]
--- chatapp/views.py
+++ chatapp/views.py
@@ -32,17 +32,43 @@ def process_json(request):
         return JsonResponse(error_message, status=400)
 
 
+@csrf_exempt
+@require_POST
+def companyinfo(request):
+    try:
+        json_data = json.loads(request.body.decode('utf-8'))
+        user_input = json_data.get('text')
+        response = funcgpt(user_input)
+        result = {'response': response}
+        return JsonResponse(result)
+
+    except json.JSONDecodeError as e:
+
+        error_message = {'error': 'Invalid JSON data  + ошибка на Frontend'}
+        return JsonResponse(error_message, status=400)
+
+
 def funcgpt(user_prompt):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "ты асистент помпошник плотформы ModMe Online"},
-            {"role": "assistant", "content": f"генерируй ответы в трех языках"
-                                             f" смотря на каком языке написал ползователь в основом это "
-                                             f"русский, узбекский и инданезийский"},
-            {"role": "user", "content": user_prompt},
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": f"ты асистент помпошник плотформы {companyinfo} если тут ModMe online"},
+                {"role": "assistant", "content": f"генерируй ответы в трех языках"
+                                                 f" смотря на каком языке написал ползователь в основом это "
+                                                 f"русский, узбекский и инданезийский"},
+                {"role": "user", "content": user_prompt},
+            ]
+        )
 
-        ]
-    )
+        return response['choices'][0]['message']['content']
 
-    return response['choices'][0]['message']['content']
+    except Exception as e:
+        # If there's an error, return it as a string to display on the frontend
+        return "📌 Лимит превышен три" \
+               "запроса в минуту!!!"
+
+
+
+
+
